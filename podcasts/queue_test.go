79a474fb914e241686d9504ea6dd06f7b50665a8
Pegasus <martingda@protonmail.com>
2020-11-19 11:48:12 -0300

    Corrected tests according to previous commit

--- podcasts/queue_test.go
+++ podcasts/queue_test.go
@@ -1,6 +1,7 @@
 package podcasts
 
 import (
+	"os"
 	"runtime"
 	"testing"
 
@@ -10,26 +11,44 @@ import (
 )
 
 type QueueTestSuite struct {
-	length int
+	length     int
+	dbPath     string
+	dbFilename string
 
 	suite.Suite
 }
 
 func (s *QueueTestSuite) SetupTest() {
 	s.length = runtime.NumCPU()
+	s.dbPath = "./test_queue"
+	s.dbFilename = "queue_test.sqlite"
+
+	err := os.Mkdir(s.dbPath, os.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (s *QueueTestSuite) BeforeTest(_, _ string) {}
 
-func (s *QueueTestSuite) TestNewQueue() {
+func (s *QueueTestSuite) TestNewUpdateQueue() {
 	assert := assert2.New(s.T())
 
-	q, err := NewQueue(s.length)
+	db, err := NewDB(s.dbPath, "new_update_queue_"+s.dbFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		_ = db.Close()
+	}()
+
+	q, err := NewUpdateQueue(db, s.length)
 
 	assert.NoError(err, "the queue should be created correctly")
 	assert.NotNil(q, "the returned Queue instance shouldn't be nil")
 
-	q, err = NewQueue(-1)
+	q, err = NewUpdateQueue(db, -1)
 
 	if assert.Error(err, "if the argument that corresponds to the length of the queue is a negative"+
 		" number or 0, an error should be returned") {
@@ -41,7 +60,12 @@ func (s *QueueTestSuite) TestNewQueue() {
 
 func (s *QueueTestSuite) AfterTest(_, _ string) {}
 
-func (s *QueueTestSuite) TearDownTest() {}
+func (s *QueueTestSuite) TearDownTest() {
+	err := os.RemoveAll(s.dbPath)
+	if err != nil {
+		panic(err)
+	}
+}
 
 func TestQueueTestSuite(t *testing.T) {
 	suite.Run(t, new(QueueTestSuite))
