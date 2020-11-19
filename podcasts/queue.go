1780a259dd98351fd08981e62bf1e8e1721cd93c
Pegasus <martingda@protonmail.com>
2020-11-19 11:37:18 -0300

    Implemented worker's logic (tests to-do)

--- podcasts/queue.go
+++ podcasts/queue.go
@@ -3,45 +3,130 @@ package podcasts
 import (
 	"github.com/joomcode/errorx"
 	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type Job struct {
-	PodcastID int
-
-	// TODO
+	Podcast *Podcast
 }
 
-type Queue struct {
-	q chan Job
+type UpdateQueue struct {
+	dbInstance *Database
+	q          chan Job
 }
 
-func NewQueue(length int) (*Queue, error) {
+func NewUpdateQueue(db *Database, length int) (*UpdateQueue, error) {
 	if length < 1 {
 		return nil, errorx.IllegalArgument.New("the length of the queue should be at least 1")
 	}
 
-	q := &Queue{
-		q: make(chan Job),
+	if db == nil {
+		return nil, errorx.IllegalState.New("the instance of the database is nil")
+	}
+
+	q := UpdateQueue{
+		q:          make(chan Job),
+		dbInstance: db,
 	}
 
 	for i := 0; i < length; i++ {
-		go worker(i, q.q)
+		go q.worker(i)
 	}
 
-	return q, nil
+	return &q, nil
 }
 
-func (q *Queue) Send(job *Job) {
+func (q *UpdateQueue) Send(job *Job) {
 	q.q <- *job
 }
 
-func worker(id int, q chan Job) {
-	log.Debugln("Starting worker", id)
+/*
+func NewJob(p *Podcast) *Job {
+
+}
+*/
+func (q *UpdateQueue) worker(id int) {
+	log.WithField("worker", id).Debug("Starting worker")
 
 	for {
-		job := <-q
-		log.Debugf("[worker %d] New job received. Updating podcast %d\n", id, job.PodcastID)
+		job := <-q.q
+		receivedTime := time.Now()
 
-		// TODO Update feed
+		log.WithFields(log.Fields{
+			"worker":      id,
+			"podcastID":   job.Podcast.ID,
+			"podcastFeed": job.Podcast.FeedLink,
+		}).Info("New job received. Updating podcast")
+
+		eps, err := job.Podcast.GetEpisodes()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"worker":      id,
+				"podcastID":   job.Podcast.ID,
+				"podcastFeed": job.Podcast.FeedLink,
+				"error":       errorx.Decorate(err, "error when getting episodes of the podcast"),
+			}).Error("Can't get the episodes of the podcast")
+
+			continue
+		}
+
+		for _, e := range *eps {
+			exists, err := q.dbInstance.EpisodeExists(e.GUID)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"worker":      id,
+					"podcastID":   job.Podcast.ID,
+					"podcastFeed": job.Podcast.FeedLink,
+					"episodeGUID": e.GUID,
+					"error":       errorx.Decorate(err, "error when trying to check if an episode exists"),
+				}).Error("Can't get the episodes of the podcast")
+
+				continue
+			}
+
+			if exists {
+				log.WithFields(log.Fields{
+					"worker":      id,
+					"podcastID":   job.Podcast.ID,
+					"podcastFeed": job.Podcast.FeedLink,
+					"episodeGUID": e.GUID,
+				}).Debug("Episode already on the database, skipping")
+
+				continue
+			}
+
+			err = q.dbInstance.InsertEpisode(&e)
+			if err != nil {
+				log.WithFields(log.Fields{
+					"worker":      id,
+					"podcastID":   job.Podcast.ID,
+					"podcastFeed": job.Podcast.FeedLink,
+					"episodeGUID": e.GUID,
+					"error":       errorx.Decorate(err, "error when trying to save the episode on the database"),
+				}).Error("Can't save the episode on the database")
+
+				continue
+			}
+
+			err = q.dbInstance.UpdatePodcastLastCheck(job.Podcast.ID, time.Now())
+			if err != nil {
+				log.WithFields(log.Fields{
+					"worker":      id,
+					"podcastID":   job.Podcast.ID,
+					"podcastFeed": job.Podcast.FeedLink,
+					"error": errorx.Decorate(err, "error when trying to update the column "+
+						"'last_check' in the database"),
+				}).Error("Can't update the LastCheck time in the database")
+
+				continue
+			}
+
+			log.WithFields(log.Fields{
+				"worker":         id,
+				"podcastID":      job.Podcast.ID,
+				"podcastFeed":    job.Podcast.FeedLink,
+				"updateDuration": time.Since(receivedTime).String(),
+			}).Info("Job finished. Podcast updated correctly")
+		}
 	}
 }
