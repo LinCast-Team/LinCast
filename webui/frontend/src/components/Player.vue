d72b81e2720fa50411ddc4159bf23a8f84f11d9f
Pegasus <martingda@protonmail.com>
2020-12-03 16:37:23 -0300

    Implementation of floating mini player

--- webui/frontend/src/components/Player.vue
+++ webui/frontend/src/components/Player.vue
@@ -1,32 +1,72 @@
 <template>
-<div class="flex flex-col border-2 border-solid transition-colors duration-500 border-gray-100 bg-gray-50 dark:border-gray-700 dark:bg-gray-900 z-100 shadow-lg font-sans" :class="{ 'fixed bottom-0 right-0 left-0 top-0 rounded-t-xl': !expanded }">
-  <h1 class="text-2xl my-8 dark:text-gray-300">Playing Now</h1>
-  <div class="flex flex-col text-center items-center mb-6">
-    <img :src="artworkSrc" :alt="podcastTitle + '\'s artwork'" class="w-64 h-64 md:w-80 md:h-80 mx-1 rounded-md shadow-lg">
-    <div class="grid grid-cols-4 grid-flow-col gap-2 items-center justify-items-center my-4 md:my-8 dark:text-gray-100">
-      <div class="" data-feather="share-2"></div>
-      <div class="col-span-2 text-center">
-        <p class="truncate text-black dark:text-gray-100 uppercase font-extrabold text-xl">{{ podcastTitle }}</p>
-        <p class="truncate text-gray-500 dark:text-gray-400">{{ episodeTitle }}</p>
-      </div>
-      <div class="" data-feather="more-vertical"></div>
+<div
+  class="flex border-2 border-solid transition-colors duration-500 border-gray-100 bg-gray-50 dark:border-gray-700 dark:bg-gray-900 z-100 shadow-lg font-sans"
+  :class="{
+    'fixed bottom-0 right-0 left-0 rounded-t-sm flex-row items-center': !expanded,
+    'flex-col': expanded,
+  }"
+>
+  <h1 v-show="expanded" class="text-2xl my-8 dark:text-gray-300">Playing Now</h1>
+
+  <img
+    :src="artworkSrc"
+    :alt="podcastTitle + '\'s artwork'"
+    class="self-center rounded-md shadow-lg"
+    :class="{
+      'w-60 h-60 md:w-80 md:h-80 mx-auto': expanded,
+      'w-12 h-12 flex-none m-4': !expanded,
+    }"
+  >
+
+  <div
+    class=" text-center dark:text-gray-100"
+    :class="{
+      'flex flex-row justify-around mt-4 mb-6 md:mt-6 md:mb-8 sm:mx-14 md:mx-20': expanded,
+      'flex-grow': !expanded,
+    }"
+  >
+    <div v-show="expanded" class="flex-none justify-self-center self-center mx-6 md:mx-14" data-feather="share-2"></div>
+    <div class="" :class="{ 'flex-grow text-center justify-self-center': expanded, 'text-left': !expanded }">
+      <p
+        class="truncate text-black dark:text-gray-100 uppercase "
+        :class="{
+          'font-extrabold text-xl': expanded,
+          'font-bold text-base': !expanded,
+        }"
+      >{{ podcastTitle }}</p>
+      <p
+        class="truncate text-gray-500 dark:text-gray-400"
+        :class="{
+          'text-sm': !expanded
+        }"
+      >{{ episodeTitle }}</p>
     </div>
+    <div v-show="expanded" class="flex-none justify-self-center self-center mx-6 md:mx-12" data-feather="more-vertical"></div>
   </div>
-  <div id="waveform" class="bg-transparent mx-4"></div>
-  <div class="bg-transparent text-black transition-colors duration-500 dark:text-gray-100 py-4 px-1 sm:px-3 lg:px-1 xl:px-3 my-auto md:mx-20 grid grid-cols-5 items-center">
-    <button class="mx-auto rounded-full">
+
+  <div v-show="expanded" id="waveform" class="bg-transparent mx-4"></div>
+
+  <div
+    class="bg-transparent text-black transition-colors duration-500 dark:text-gray-100    "
+    :class="{
+      'grid grid-cols-5 items-center md:mx-20 py-4 px-1 sm:px-3 lg:px-1 xl:px-3 my-auto': expanded,
+      'self-center': !expanded,
+    }"
+  >
+    <button v-show="expanded" class="mx-auto rounded-full">
       <span class="w-6 h-6 md:w-10 md:h-10" data-feather="skip-back" stroke-width="1.5"></span>
     </button>
-    <button @click="wavesurfer.skipBackward(15)" class="mx-auto rounded-full">
+    <button v-show="expanded" @click="wavesurfer.skipBackward(15)" class="mx-auto rounded-full">
       <span class="w-8 h-8 md:w-12 md:h-12" data-feather="rotate-ccw" stroke-width="1.5"></span>
     </button>
-    <button @click="wavesurfer.playPause()" class="mx-auto rounded-full">
-      <span class="w-16 h-16 md:w-20 md:h-20" stroke-width="0.8" data-feather="play-circle"></span>
+    <button @click="wavesurfer.playPause()" class=" mx-4 rounded-full" :class="{ 'mx-auto': expanded, 'flex-none': !expanded }">
+      <span v-if="expanded" class="w-16 h-16 md:w-20 md:h-20" stroke-width="0.8" data-feather="play-circle"></span>
+      <span v-else class="w-9 h-9" stroke-width="1.0" data-feather="play"></span>
     </button>
-    <button @click="wavesurfer.skipForward(15)" class="mx-auto rounded-full">
+    <button v-show="expanded" @click="wavesurfer.skipForward(15)" class="mx-auto rounded-full">
       <span class="w-8 h-8 md:w-12 md:h-12" data-feather="rotate-cw" stroke-width="1.5"></span>
     </button>
-    <button class="mx-auto rounded-full">
+    <button v-show="expanded" class="mx-auto rounded-full">
       <span class="w-6 h-6 md:w-10 md:h-10" data-feather="skip-forward" stroke-width="1.5"></span>
     </button>
   </div>
@@ -68,7 +108,7 @@ export default {
     expanded: {
       type: Boolean,
       required: false,
-      default: false,
+      default: true,
     },
   },
   setup(props) {
