17ec0df49c8fe53edf76957da15e2cd0fac6f6db
Pegasus <martingda@protonmail.com>
2020-12-03 11:30:38 -0300

    Added dark mode color palette

--- webui/frontend/src/components/Player.vue
+++ webui/frontend/src/components/Player.vue
@@ -1,19 +1,19 @@
 <template>
-<div class="flex flex-col border-2 border-solid border-gray-100 bg-gray-50 rounded-t-xl z-100 shadow-lg font-sans" :class="{ 'fixed bottom-0 right-0 left-0 top-0': !expanded }">
-  <h1 class="text-2xl my-8">Playing Now</h1>
+<div class="flex flex-col border-2 border-solid transition-colors duration-500 border-gray-100 bg-gray-50 dark:border-gray-700 dark:bg-gray-900 z-100 shadow-lg font-sans" :class="{ 'fixed bottom-0 right-0 left-0 top-0 rounded-t-xl': !expanded }">
+  <h1 class="text-2xl my-8 dark:text-gray-300">Playing Now</h1>
   <div class="flex flex-col text-center items-center mb-6">
     <img :src="artworkSrc" :alt="podcastTitle + '\'s artwork'" class="w-64 h-64 md:w-80 md:h-80 mx-1 rounded-md shadow-lg">
-    <div class="grid grid-cols-4 grid-flow-col gap-2 items-center justify-items-center my-4 md:my-8">
+    <div class="grid grid-cols-4 grid-flow-col gap-2 items-center justify-items-center my-4 md:my-8 dark:text-gray-100">
       <div class="" data-feather="share-2"></div>
       <div class="col-span-2 text-center">
-        <p class="truncate text-black uppercase font-extrabold text-xl">{{ podcastTitle }}</p>
-        <p class="truncate text-gray-500">{{ episodeTitle }}</p>
+        <p class="truncate text-black dark:text-gray-100 uppercase font-extrabold text-xl">{{ podcastTitle }}</p>
+        <p class="truncate text-gray-500 dark:text-gray-400">{{ episodeTitle }}</p>
       </div>
       <div class="" data-feather="more-vertical"></div>
     </div>
   </div>
-  <div id="waveform" class="bg-gray-50 mx-4"></div>
-  <div class="bg-gray-50 text-black transition-colors duration-500 dark:bg-gray-900 dark:text-white py-4 px-1 sm:px-3 lg:px-1 xl:px-3 my-auto md:mx-20 grid grid-cols-5 items-center">
+  <div id="waveform" class="bg-transparent mx-4"></div>
+  <div class="bg-transparent text-black transition-colors duration-500 dark:text-gray-100 py-4 px-1 sm:px-3 lg:px-1 xl:px-3 my-auto md:mx-20 grid grid-cols-5 items-center">
     <button class="mx-auto rounded-full">
       <span class="w-6 h-6 md:w-10 md:h-10" data-feather="skip-back" stroke-width="1.5"></span>
     </button>
