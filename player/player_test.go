a97820a4d7cdb2805b2436fa9aeeeefab668bbb5
Pegasus <martingda@protonmail.com>
2020-12-30 22:39:18 -0300

    Initialized tests

--- player/player_test.go
+++ player/player_test.go
@@ -1,1 +1,47 @@
 package player
+
+import (
+	"github.com/stretchr/testify/suite"
+	"lincast/database"
+	"testing"
+)
+
+type SynchronizerTestSuite struct {
+	dbPath     string
+	dbFilename string
+	dbInstance *database.Database
+
+	suite.Suite
+}
+
+func (s *SynchronizerTestSuite) SetupTest() {
+	s.dbPath = "./test_synchronizer"
+}
+
+func (s *SynchronizerTestSuite) BeforeTest(_, _ string) {
+
+}
+
+func (s *SynchronizerTestSuite) TestNew() {
+
+}
+
+func (s *SynchronizerTestSuite) TestUpdateProgress() {
+
+}
+
+func (s *SynchronizerTestSuite) TestGetProgress() {
+
+}
+
+func (s *SynchronizerTestSuite) AfterTest(_, _ string) {
+
+}
+
+func (s *SynchronizerTestSuite) TearDownTest() {
+
+}
+
+func TestSynchronizerTestSuite(t *testing.T) {
+	suite.Run(t, new(SynchronizerTestSuite))
+}
