7e76edcdd25d0b9fad637dc9adaf2bed6762e1d4
Bruno <40237383+brunonra-dev@users.noreply.github.com>
2021-01-23 00:35:13 -0300

    Init discover - added search bar

--- webui/frontend/src/views/Discover.vue
+++ webui/frontend/src/views/Discover.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="">
-    <p>Discover view</p>
+  <div class="flex justify-center">
+    <div class="mt-20 p-0.5 rounded-md bg-gradient-to-r from-indigo-700 to-green-700">
+      <input class="p-2 rounded-md bg-gray-800 " type="text" name="" placeholder="Search" id="">
+    </div>
   </div>
 </template>
+<script>
+export default {
+};
+</script>
+<style lang="scss">
+</style>
