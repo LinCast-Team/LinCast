9e3c095a5f4f9cac7d099e76366cdc928e753f76
Bruno <40237383+brunonra-dev@users.noreply.github.com>
2021-01-26 23:24:15 -0300

    Discover - Category Component Implementation
    Co-authored-by: Martín Díaz <43992893+Pegasus8@users.noreply.github.com>

---
+++ webui/frontend/src/components/discover/Category.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="m-2 p-0.5 rounded-md bg-gradient-to-r from-indigo-700 to-green-700 w-64">
+    <router-link tag="a" to="" class="p-3 w-64 text-gray-200 inline-block">{{ title }}</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style>
+</style>
--- webui/frontend/src/views/Discover.vue
+++ webui/frontend/src/views/Discover.vue
@@ -1,12 +1,44 @@
 <template>
-  <div class="flex justify-center">
-    <div class="mt-20 p-0.5 rounded-md bg-gradient-to-r from-indigo-700 to-green-700">
-      <input class="p-2 rounded-md bg-gray-800 " type="text" name="" placeholder="Search" id="">
+  <div class="flex flex-col items-center justify-center font-sans">
+    <div>
+      <div class="mt-20 p-0.5 rounded-md bg-gradient-to-r from-indigo-700 to-green-700">
+        <div class="p-3 w-64 bg-gray-800 flex">
+          <div v-html="searchIcon"></div>
+          <input
+            class="rounded-md bg-transparent border-0 placeholder-white focus:placeholder-gray-500 focus:outline-none"
+            type="text"
+            role="search"
+            name="Search a podcast"
+            placeholder="Search"
+          >
+        </div>
+      </div>
     </div>
+    <div class="m-2 p-0.5 ">
+      <h3 class="p-2 w-64 text-lg text-green-600 inline-block font-semibold">Categories</h3>
+    </div>
+    <category title="Arts"/>
+    <category title="Business"/>
+    <category title="News"/>
+    <category title="Music"/>
+    <category title="Technology"/>
   </div>
 </template>
 <script>
+import { computed } from 'vue';
+import feather from 'feather-icons';
+import Category from '@/components/discover/Category.vue';
+
 export default {
+  components: {
+    Category,
+  },
+  setup() {
+    const searchIcon = computed(() => feather.icons.search.toSvg({ 'stroke-width': 1.5, class: 'w-5 h-5 md:w-12 md:h-12 text-white mr-2' }));
+    return {
+      searchIcon,
+    };
+  },
 };
 </script>
 <style lang="scss">
