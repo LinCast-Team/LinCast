973593c68f1c11f2595285f9c58a4ce58791a601
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-04-26 22:43:11 -0300

    Added component to show preview of subscriptions
    (unfinished)

---
+++ webui/frontend/src/components/home/SubscriptionsPreview.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="flex flex-row justify-between">
+      <h2 class="text-primary-dt text-xl font-semibold">Subscriptions</h2>
+      <router-link
+        :to="{ name: 'Subscriptions' }"
+        class="link text-sm self-center"
+        >Show more</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {};
+</script>
+
+<style lang='scss' scoped>
+</style>
