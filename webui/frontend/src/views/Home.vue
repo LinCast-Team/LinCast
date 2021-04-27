88e496777c59ece38f79ef3cc9fc27e4ef38b810
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-04-26 22:46:21 -0300

    Usage of the component SubscriptionsPreview

--- webui/frontend/src/views/Home.vue
+++ webui/frontend/src/views/Home.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="">
-    <p>Home view</p>
+    <div class="my-2 mx-6">
+      <subscriptions-preview/>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
+import SubscriptionsPreview from '@/components/home/SubscriptionsPreview.vue';
+
 export default {
+  components: {
+    SubscriptionsPreview,
+  },
 };
 </script>
