3f4a8de2965508eb66d585c25b09b5e173e8004d
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-04-26 22:44:39 -0300

    Implemented component Header of view Subs.

---
+++ webui/frontend/src/components/subscriptions/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex flex-row justify-between m-2 p-2">
+    <div v-html="backIcon"></div>
+    <h2 class="text-primary-dt text-xl font-semibold">{{ title }}</h2>
+    <div v-html="moreIcon"></div>
+  </div>
+</template>
+
+<script lang='ts'>
+import { computed } from 'vue';
+import feather from 'feather-icons';
+
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  setup() {
+    const backIcon = computed(() => feather.icons['arrow-left'].toSvg({ 'stroke-width': 1.5, class: 'text-secondary-dt w-7 h-7 mx-4' }));
+    const moreIcon = computed(() => feather.icons['more-vertical'].toSvg({ 'stroke-width': 1.5, class: 'text-secondary-dt w-7 h-7 mx-4' }));
+
+    return {
+      backIcon,
+      moreIcon,
+    };
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+</style>
