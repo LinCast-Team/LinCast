dc5e43e57252bf5c1049717ec20277c581508e06
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-05-10 02:55:17 -0300

    Revert "Moved scripts related with the API to vuex store"
    This reverts commit db671dfce2426aadadc53490195c888ca192a088.

---
+++ webui/frontend/src/api/subscriptions.ts
@@ -0,0 +1,111 @@
+export interface UserPodcasts {
+  subscribed: Array<Podcast>;
+  unsubscribed: Array<Podcast>;
+}
+
+export interface Podcast {
+  id: number;
+  subscribed: boolean;
+  authorName: string;
+  authorEmail: string;
+  title: string;
+  description: string;
+  categories: Array<string>;
+  imageURL: string;
+  imageTitle: string;
+  link: string;
+  feedLink: string;
+  feedType: string;
+  feedVersion: string;
+  language: string;
+  updated: Date;
+  lastCheck: Date;
+  added: Date;
+}
+
+export interface Episode {
+  id: number;
+  parentPodcastID: number;
+  title: string;
+  description: string;
+  link: string;
+  authorName: string;
+  guid: string;
+  imageURL: string;
+  imageTitle: string;
+  categories: Array<string>;
+  enclosureURL: string;
+  enclosureLength: string;
+  enclosureType: string;
+  season: string;
+  published: Date;
+  updated: Date;
+  played: boolean;
+  currentProgress: Date;
+}
+
+export const getUserPodcasts = async (): Promise<UserPodcasts> => {
+  const response = await fetch('/api/v0/podcasts/user', {
+    method: 'GET'
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
+
+  const data: UserPodcasts = await response.json();
+
+  return data;
+};
+
+export const getUserPodcastDetails = async (id: number): Promise<Podcast> => {
+  const response = await fetch(`/api/v0/podcasts/${id}/details`, {
+    method: 'GET'
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
+
+  const data: Podcast = await response.json();
+
+  return data;
+};
+
+export const getUserPodcastEpisodes = async (podcastID: number): Promise<Array<Episode>> => {
+  const response = await fetch(`/api/v0/podcasts/${podcastID}/episodes`, {
+    method: 'GET'
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
+
+  const data: Array<Episode> = await response.json();
+
+  return data;
+};
+
+export const subscribe = async (feedURL: string) => {
+  const response = await fetch('/api/v0/podcasts/subscribe', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ url: feedURL }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
+};
+
+export const unsubscribe = async (podcastID: number) => {
+  const response = await fetch(`/api/v0/podcasts/unsubscribe?id=${podcastID}`, {
+    method: 'PUT',
+  });
+
+  if (!response.ok) {
+    throw new Error(`Request failed with status code ${response.status}`);
+  }
+};
