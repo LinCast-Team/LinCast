9edbaa6556f2e16ddff13cd0791a0bea6a6819ee
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-07-03 02:02:13 -0300

    test(api): check of `ContentLength` headers
    On the tests of `SubscribeToPodcastHandler`.

--- webui/handlers/podcasts_test.go
+++ webui/handlers/podcasts_test.go
@@ -30,16 +30,19 @@ func TestSubscribeToPodcastHandler(t *testing.T) {
 
 	assert.Equal(http.StatusCreated, r.StatusCode, "The status code returned on the subscription of a new podcast should be 201 Created")
 	assert.Equal("", r.Header.Get("Content-Type"), "Since the response should not return a body, the 'Content-Type' headers should not be there")
+	assert.Equal(0, r.ContentLength, "The ContentLength of the response should be 0, since it should not have a body")
 
 	r = testUtils.NewRequest(mng.SubscribeToPodcastHandler, method, "", testUtils.NewBody(t, body))
 
 	assert.Equal(http.StatusNoContent, r.StatusCode, "The status code returned on the subscription of a podcast that is already on the database should be 204 No Content")
 	assert.Equal("", r.Header.Get("Content-Type"), "Since the response should not return a body, the 'Content-Type' headers should not be there")
+	assert.Equal(0, r.ContentLength, "The ContentLength of the response should be 0, since it should not have a body")
 
 	body.Url = "abc123"
 	r = testUtils.NewRequest(mng.SubscribeToPodcastHandler, method, "", testUtils.NewBody(t, body))
 
 	assert.Equal(http.StatusBadRequest, r.StatusCode, "The status code returned if the feed provided is invalid should be 400 Bad Request")
-	assert.Equal("text/plain; charset=utf-8", r.Header.Get("Content-Type"), "Since the response should contain an error msg in plain text, the 'Content-Type' headers should be "+
-		"'text/plain; charset=utf-8'")
+	assert.Equal("text/plain; charset=utf-8", r.Header.Get("Content-Type"), "Since the response should contain an error msg in plain text, the "+
+		"'Content-Type' headers should be 'text/plain; charset=utf-8'")
+	assert.NotEqual(0, r.ContentLength, "The ContentLength of the response should not be 0, since the body must contain the description of the error")
 }
