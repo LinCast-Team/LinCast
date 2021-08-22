453afacdda8356df7a4c96bc55a3262f1166be49
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-08-21 21:02:56 -0300

    test(api): added test for function `NewManager`

---
+++ webui/handlers/manager_test.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"testing"
+
+	"lincast/database"
+
+	assert2 "github.com/stretchr/testify/assert"
+)
+
+
+func TestNewManager(t *testing.T) {
+	assert := assert2.New(t)
+	tempDir := t.TempDir()
+	db, err := database.New(tempDir, "test.db")
+	if err != nil {
+		assert.FailNow(err.Error())
+	}
+	
+	mng := NewManager(db)
+
+	assert.NotNil(mng, "A valid instance of Manager should be returned")
+}
