afc4fc67cc507351769dba4994f2e5a33b3c100e
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-08-26 14:31:03 -0300

    test(api): testing of `QueueHandler` PUT

--- webui/handlers/queue_test.go
+++ webui/handlers/queue_test.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"net/http"
 	"testing"
+	"time"
 
 	"lincast/database"
 	"lincast/models"
 	testUtils "lincast/utils/testing"
 
 	assert2 "github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
 )
 
 func TestQueueHandler_GET(t *testing.T) {
@@ -67,4 +69,73 @@ func TestQueueHandler_GET(t *testing.T) {
 	assert.Equal(http.StatusOK, r.StatusCode)
 	assert.Equal("application/json", r.Header.Get("Content-Type"))
 	assert.Equal(expectedQueue, receivedQueue)
+}
+
+func TestQueueHandler_PUT(t *testing.T) {
+	assert := assert2.New(t)
+	tempDir := t.TempDir()
+	db, err := database.New(tempDir, "test.db")
+	if err != nil {
+		assert.FailNow(err.Error())
+	}
+	mng := NewManager(db)
+	method := "PUT"
+
+	expectedQueue := []models.QueueEpisode{
+		{
+			Position:  1,
+			PodcastID: 10,
+			EpisodeID: "guid1",
+			Model: gorm.Model{
+				ID: 1,
+			},
+		},
+		{
+			Position:  2,
+			PodcastID: 1,
+			EpisodeID: "guid2",
+			Model: gorm.Model{
+				ID: 2,
+			},
+		},
+		{
+			Position:  3,
+			PodcastID: 18,
+			EpisodeID: "guid3",
+			Model: gorm.Model{
+				ID: 3,
+			},
+		},
+	}
+
+	r := testUtils.NewRequest(mng.QueueHandler, method, "", testUtils.NewBody(t, &expectedQueue))
+
+	var queueOnDB []models.QueueEpisode
+	res := db.Find(&queueOnDB)
+	if res.Error != nil {
+		assert.FailNow(res.Error.Error())
+	}
+
+	for i := range queueOnDB {
+		queueOnDB[i].Model.CreatedAt = time.Time{}
+		queueOnDB[i].Model.UpdatedAt = time.Time{}
+	}
+
+	assert.Equal(http.StatusCreated, r.StatusCode)
+	assert.Equal("", r.Header.Get("Content-Type"))
+	assert.Equal(expectedQueue, queueOnDB)
+
+	// The usage of a repeated position should cause the rejection of the request
+	wrongQueue := expectedQueue
+	wrongQueue[1].Position = 1
+	r = testUtils.NewRequest(mng.QueueHandler, method, "", testUtils.NewBody(t, &wrongQueue))
+
+	assert.Equal(http.StatusBadRequest, r.StatusCode)
+	assert.Equal("text/plain; charset=utf-8", r.Header.Get("Content-Type"))
+
+	wrongQueue2 := "[{'foo': 'bar', '1': 2}]"
+	r = testUtils.NewRequest(mng.QueueHandler, method, "", testUtils.NewBody(t, &wrongQueue2))
+
+	assert.Equal(http.StatusBadRequest, r.StatusCode)
+	assert.Equal("text/plain; charset=utf-8", r.Header.Get("Content-Type"))
 }
