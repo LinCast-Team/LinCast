1af655c4616bb1a08636da051d264ee440259aa3
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2021-08-31 15:47:44 -0300

    Added a message when there's no episodes to show

--- webui/frontend/src/components/subscriptions/tabs/NewEspisodesTab.vue
+++ webui/frontend/src/components/subscriptions/tabs/NewEspisodesTab.vue
@@ -1,20 +1,43 @@
 <template>
-  <h2 class="p-2 pt-3 text-secondary-dt">Today</h2>
-  <episode-item
-    title = "Episode"
-    author = "Author"
-    update = "Episode"
-    resume = "Officia consequat sit nostrud nulla aliquip non. Dolore consequat officia mollit id consectetur. Enim deserunt do laboris minim occaecat nulla labore ad enim. Non et id minim ex excepteur. Occaecat aliqua veniam excepteur tempor voluptate."
-    duration = "10:23"
-  />
+<div class="flex px-2 py-5">
+  <div v-if="episodes.length > 0">
+    <h2 class="p-2 pt-3 text-secondary-dt">Today</h2>
+    <!-- FIX Some props do not match with the fields of the object -->
+    <episode-item
+      v-for="e in episodes"
+      :title="e.title"
+      :author="e.podcastTitle"
+      :resume="e.description"
+      :duration="e.duration"
+      :key="e.id"
+    />
+    <!-- <episode-item
+      title = "Episode"
+      author = "Author"
+      resume = "Officia consequat sit nostrud nulla aliquip non. Dolore consequat officia mollit id consectetur. Enim deserunt do laboris minim occaecat nulla labore ad enim. Non et id minim ex excepteur. Occaecat aliqua veniam excepteur tempor voluptate."
+      duration = "10:23"
+    /> -->
+  </div>
+  <div v-else class="mx-auto">
+    <p class="self-center text-secondary-dt">Looks like there is nothing here...</p>
+  </div>
+</div>
 </template>
 
 <script>
+import { ref } from 'vue';
 import EpisodeItem from '@/components/library/EpisodeItem.vue';
 
 export default {
   components: {
     EpisodeItem,
+  },
+  setup() {
+    const episodes = ref([]);
+
+    return {
+      episodes,
+    };
   },
 };
 </script>
