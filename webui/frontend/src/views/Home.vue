d742bed862f41f0d30b0cd97df952bdf72d9d124
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2021-09-03 22:44:26 -0300

    feat(home-view): added section `ForYou`

--- webui/frontend/src/views/Home.vue
+++ webui/frontend/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div class="">
-    <div class="my-2 mx-6">
-      <subscriptions-preview/>
-    </div>
+  <div class="m-6 space-y-5">
+    <for-you/>
+    <subscriptions-preview/>
   </div>
 </template>
 
 <script lang="ts">
 import SubscriptionsPreview from '@/components/home/SubscriptionsPreview.vue';
+import ForYou from '@/components/home/ForYou.vue';
 
 export default {
   components: {
     SubscriptionsPreview,
+    ForYou,
   },
 };
 </script>
