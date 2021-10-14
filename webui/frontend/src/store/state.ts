fe65b595bb9dcde76e630a52d2f3dc950a2a0325
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2021-10-14 15:26:09 -0300

    refactor(vuex): storage of the player progress in the state

--- webui/frontend/src/store/state.ts
+++ webui/frontend/src/store/state.ts
@@ -5,6 +5,7 @@ export const state = {
   userPodcasts: new Array<Podcast>(),
   latestEpisodes: new Array<Episode>(),
   playbackInfo: {} as PlaybackInfo,
+  playerProgress: 0,
 };
 
 export type State = typeof state;
