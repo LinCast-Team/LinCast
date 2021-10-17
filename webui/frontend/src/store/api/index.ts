fc8b7ab510fc57073aae7c3227cd6938714c9747
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2021-10-17 00:47:21 -0300

    feat(api-ui): exported classes API classes

---
+++ webui/frontend/src/store/api/index.ts
@@ -0,0 +1,9 @@
+import PlayerAPI from './player';
+import PlayerQueueAPI from './queue';
+import SubscriptionsAPI from './subscriptions';
+
+export default {
+  PlayerAPI,
+  PlayerQueueAPI,
+  SubscriptionsAPI,
+};
