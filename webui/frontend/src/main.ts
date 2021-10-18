5410a71992c4e63fe5c4abf5cb9fe86e60a4b34a
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2021-10-17 22:04:21 -0300

    refactor(vuex): removed vuex store

--- webui/frontend/src/main.ts
+++ webui/frontend/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { store } from './store';
 import '@/assets/css/tailwind.css';
 
 const app = createApp(App);
 
-app.use(store);
 app.use(router);
 
 router.isReady().then(() => {
