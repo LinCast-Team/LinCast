5d1e2247b68e224b5147ca6f004415d530bd7230
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2021-12-13 21:59:43 -0300

    build(storybook): added accessibility addon

--- webui/frontend/.storybook/main.js
+++ webui/frontend/.storybook/main.js
@@ -9,6 +9,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-postcss",
+    "@storybook/addon-a11y",
   ],
   "framework": "@storybook/vue3",
   webpackFinal: async (config, { configType }) => {
