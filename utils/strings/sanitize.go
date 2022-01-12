8532a3ad1b6057a24977a6da55c26530f7f3dcaa
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2022-01-12 17:05:42 -0300

    feat(utils): added function to sanitize strings before logging
    The named function is Sanitize.

---
+++ utils/strings/sanitize.go
@@ -0,0 +1,16 @@
+package strings
+
+import (
+	"strings"
+)
+
+// Sanitize sanitizes the given string, removing elements that can be dangerous to log (e.g.: line endings).
+// References:
+// - https://owasp.org/www-community/attacks/Log_Injection
+// - https://cwe.mitre.org/data/definitions/117.html
+func Sanitize(s string) string {
+	s = strings.Replace(s, "\n", "", -1)
+	s = strings.Replace(s, "\r", "", -1)
+
+	return s
+}
