d51c0fd1057f09b94061c0221428b75134881674
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2022-01-12 23:04:55 -0300

    refactor(api-middleware): apply sanitization of user agent before logging

--- webui/middleware.go
+++ webui/middleware.go
@@ -3,13 +3,15 @@ package webui
 import (
 	"net/http"
 
+	"lincast/utils/safe"
+
 	log "github.com/sirupsen/logrus"
 )
 
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.WithFields(log.Fields{
-			"userAgent":     r.UserAgent(),
+			"userAgent":     safe.Sanitize(r.UserAgent()),
 			"remoteAddr":    r.RemoteAddr,
 			"tls":           r.TLS != nil,
 			"contentLength": r.ContentLength,
