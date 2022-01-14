795466eb0c4fe16e265a4f6062518a504d519a20
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2022-01-14 17:56:07 -0300

    feat(api-middleware): log the requested URL
    This time, more safety, since the method String of the struct url.URL takes care of escaping the raw URL of non-ASCII characters.

--- webui/middleware.go
+++ webui/middleware.go
@@ -11,6 +11,7 @@ import (
 func loggingMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.WithFields(log.Fields{
+			"url":           r.URL.String(),
 			"userAgent":     safe.Sanitize(r.UserAgent()),
 			"remoteAddr":    r.RemoteAddr,
 			"tls":           r.TLS != nil,
