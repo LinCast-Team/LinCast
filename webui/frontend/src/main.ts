8e985f3e6474e7b3713f25e18b3cf045f553881f
Pegasus8 <43992893+Pegasus8@users.noreply.github.com>
2022-01-16 22:26:57 -0300

    feat(ui-state): initialized Pinia

--- webui/frontend/src/main.ts
+++ webui/frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import '@/assets/css/tailwind.css';
@@ -6,6 +7,7 @@ import '@/assets/css/tailwind.css';
 const app = createApp(App);
 
 app.use(router);
+app.use(createPinia());
 
 router.isReady().then(() => {
   app.mount('#app');
