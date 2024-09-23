7647d209df87eba505919613a2c537a44792c102
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-09-22 21:38:05 -0300

    Add methods to podcasts' repository

--- repositories/podcast.go
+++ repositories/podcast.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"errors"
 	"lincast/models"
 
 	"gorm.io/gorm"
@@ -8,9 +9,11 @@ import (
 
 type PodcastRepository interface {
 	GetById(id uint) (*models.Podcast, error)
+	GetByFeed(feedUrl string) (*models.Podcast, error)
 	Create(podcast models.Podcast) error
 	Update(podcast models.Podcast) error
 	Delete(id uint) error
+	UpdateSubscriptionStatus(userID uint, podcastID uint, subscribed bool) error
 }
 
 type podcastRepository struct {
@@ -23,36 +26,62 @@ func NewPodcastRepository(db *gorm.DB) PodcastRepository {
 	}
 }
 
-func (dao *podcastRepository) GetById(id uint) (*models.Podcast, error) {
+func (pr *podcastRepository) GetById(id uint) (*models.Podcast, error) {
 	var p models.Podcast
 
-	if err := dao.db.First(&p, id).Error; err != nil {
+	if err := pr.db.First(&p, id).Error; err != nil {
 		return nil, err
 	}
 
 	return &p, nil
 }
 
-func (dao *podcastRepository) Create(podcast models.Podcast) error {
-	if err := dao.db.Create(podcast).Error; err != nil {
+func (pr *podcastRepository) Create(podcast models.Podcast) error {
+	if err := pr.db.Create(podcast).Error; err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func (dao *podcastRepository) Update(podcast models.Podcast) error {
-	if err := dao.db.Save(podcast).Error; err != nil {
+func (pr *podcastRepository) Update(podcast models.Podcast) error {
+	if err := pr.db.Save(podcast).Error; err != nil {
 		return nil
 	}
 
 	return nil
 }
 
-func (dao *podcastRepository) Delete(id uint) error {
-	if err := dao.db.Delete(&models.Podcast{}, id).Error; err != nil {
+func (pr *podcastRepository) Delete(id uint) error {
+	if err := pr.db.Delete(&models.Podcast{}, id).Error; err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func (pr *podcastRepository) GetByFeed(feedUrl string) (*models.Podcast, error) {
+	var p = models.Podcast{
+		FeedLink: feedUrl,
+	}
+
+	// TODO check if this does work. If not, make use of the Where clause
+	if err := pr.db.First(&p).Error; err != nil {
+		return nil, err
+	}
+
+	return &p, nil
+}
+
+func (pr *podcastRepository) UpdateSubscriptionStatus(userID uint, podcastID uint, subscribed bool) error {
+	return errors.New("not implemented")
+
+	/*
+	// TODO Check if this does work
+	// Update the subscription status of a user to a podcast, by using the variable subscribed to give the value
+	if err := pr.db.Model(&models.User{Model: gorm.Model{ID: userID}}).Preload("SubscribedTo").Delete("subscribedTo", "podcast_id = ?", podcastID).Error; err != nil {
+		return err
+	}
+
+	return nil*/
 }
