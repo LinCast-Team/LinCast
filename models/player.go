035b2cc3d36985dd34cc64bed4509b429f9d37f3
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-10-13 13:41:47 -0300

    Refactor PlaybackInfo model to include UUID as primary key

--- models/player.go
+++ models/player.go
@@ -1,14 +1,24 @@
 package models
 
 import (
+	"time"
+
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 // PlaybackInfo is the structure used to store and parse the information related with the episode that is being
 // played by the player.
 type PlaybackInfo struct {
-	EpisodeID int `json:"episodeID"`
-	UserID    int `json:"userID"`
+	ID        uuid.UUID `json:"id" gorm:"type:char(36);primaryKey"`
+	EpisodeID uint       `json:"episodeID"`
+	Episode   Episode   `json:"-" gorm:"foreignKey:EpisodeID"`
+	CreatedAt time.Time `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updatedAt" gorm:"autoUpdateTime"`
+	DeletedAt time.Time `json:"deletedAt" gorm:"autoDeleteTime"`
+}
 
-	gorm.Model `json:"-"`
+func (pi *PlaybackInfo) BeforeCreate(tx *gorm.DB) (err error) {
+    pi.ID = uuid.New()
+    return
 }
