b41b5c01a402d87818a3934dba44088cbdcf7005
Martín Díaz <43992893+Pegasus8@users.noreply.github.com>
2024-10-25 11:49:40 -0300

    Added validation when creating a new User

--- internal/domain/entities/user.go
+++ internal/domain/entities/user.go
@@ -1,6 +1,7 @@
 package entities
 
 import (
+	"regexp"
 	"time"
 
 	"github.com/google/uuid"
@@ -22,16 +23,68 @@ type User struct {
 	DeletedAt       time.Time         `json:"deletedAt" gorm:"autoDeleteTime"`
 }
 
-func NewUser(username, passwordHash, email, name string) *User {
+func NewUser(username, passwordHash, email, name string) (*User, error) {
+	if err := validateUsername(username); err != nil {
+		return nil, err
+	}
+
+	if err := validateEmail(email); err != nil {
+		return nil, err
+	}
+
+	if err := validatePassword(passwordHash); err != nil {
+		return nil, err
+	}
+
 	return &User{
 		Username:     username,
 		PasswordHash: passwordHash,
 		Email:        email,
 		Name:         name,
-	}
+	}, nil
 }
 
 func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
 	u.ID = uuid.New()
 	return
+}
+
+func validateUsername(username string) error {
+	if len(username) < 3 || len(username) > 50 {
+		return ErrInvalidUsernameLength
+	}
+
+	return nil
+}
+
+// Validation related regex
+var (
+	emailRegex      = regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`)
+	hasNumberRegex  = regexp.MustCompile(`[0-9]`)
+	hasUpperRegex   = regexp.MustCompile(`[A-Z]`)
+	hasSpecialRegex = regexp.MustCompile(`[!@#$%^&*]`)
+)
+
+func validateEmail(email string) error {
+	if !emailRegex.MatchString(email) {
+		return ErrInvalidEmailFormat
+	}
+
+	return nil
+}
+
+func validatePassword(password string) error {
+	if len(password) < 8 {
+		return ErrPasswordTooShort
+	}
+
+	hasNumber := hasNumberRegex.MatchString(password)
+	hasUpper := hasUpperRegex.MatchString(password)
+	hasSpecial := hasSpecialRegex.MatchString(password)
+
+	if !hasNumber || !hasUpper || !hasSpecial {
+		return ErrPasswordRequirements
+	}
+
+	return nil
 }
